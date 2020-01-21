2e261bcf27f36fe493b4e0600faf3bbaf3584859
gorlemZ <andrea.alice.gorletta@gmail.com>
2020-01-21 18:06:00 +0100

    notes

--- src/main/scala/WriterMonad.scala
+++ src/main/scala/WriterMonad.scala
@@ -10,7 +10,7 @@ class WriterMonad {
   /**
    * cats.data.Writer is a monad
    * that lets us carry a log along with a computation,
-   * recording sequences of steps in mulঞthreaded computaঞons
+   * recording sequences of steps in multithreaded computations
    * where standard imperative logging techniques can result
    * in interleaved messages from different contexts.
    * Cats implements writer in terms of another type, WriterT
@@ -52,6 +52,43 @@ class WriterMonad {
   val (log, result) = b.run
   // log: scala.collection.immutable.Vector[String] = Vector(msg1, msg2, msg3)
   // result: Int = 123
+
+  val writer1: WriterT[Id, Vector[String], Int] = for {
+    a <- 10.pure[Logged]
+    _ <- Vector("a", "b", "c").tell
+    b <- 32.writer(Vector("x", "y", "z"))
+  } yield a + b
+  // writer1: cats.data.WriterT[cats.Id,Vector[String],Int] =
+  // WriterT((Vector(a, b, c, x, y, z),42))
+
+  /**
+   * IMPORTANT NOTE
+   * Notice that the type reported on the console is actually
+   * WriterT[Id, Vector[String], Int] instead of
+   * Writer[Vector[String], Int] as we might expect.
+   * In the spirit of code reuse, Cats implements Writer in
+   * terms of another type, WriterT
+   */
+
+  val writer2 = writer1.mapWritten(_.map(_.toUpperCase))
+  // writer2: cats.data.WriterT[cats.Id,scala.collection.immutable.Vector[String],Int]
+  // = WriterT((Vector(A, B, C, X, Y, Z),42))
+
+  /**
+   * We can transform the log in a Writer using mapWritten or both
+   * simultaneously using bimap (two parameters functions) or mapBoth
+   * (one function with two parameters)
+   */
+
+  /**
+   * CLEARING THE LOG:
+   */
+
+  writer2.reset
+  // writer5: cats.data.WriterT[cats.Id,Vector[String],Int] =
+  // WriterT((Vector(),42))
+
+
 }
 
 
